f40b48beee386adf4934de4eaaf1139613b592ee
Kavya Bajaj <bajajkavya0609@gmail.com>
2026-08-30 19:05:05 +0530

    Update traffic.c

--- SEM3/C/lab/lab4/traffic.c
+++ SEM3/C/lab/lab4/traffic.c
@@ -1,1 +1,32 @@
 
+#include <stdio.h>
+
+void main() {
+    char input;
+
+    printf("Enter R, O, or G: ");
+    scanf(" %c", &input);
+
+    switch (input) {
+        case 'R':
+        case 'r':
+            printf("STOP\n");
+            break;
+
+        case 'O':
+        case 'o':
+            printf("GET READY\n");
+            break;
+
+        case 'G':
+        case 'g':
+            printf("GO\n");
+            break;
+
+        default:
+            printf("Invalid input\n");
+            break;
+    }
+
+    
+}
